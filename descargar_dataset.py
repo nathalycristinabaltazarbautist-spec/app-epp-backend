02577ac9553b137ba1609cf681715466d0cb5fb2
nathalycristinabaltazarbautist-spec <nathalycristinabaltazarbautist@gmail.com>
2026-06-11 02:44:12 -0500

    Delete descargar_dataset.py

--- descargar_dataset.py
+++
@@ -1,8 +0,0 @@
-from roboflow import Roboflow
-
-rf = Roboflow(api_key="IJ4gG5iC6KlvDLZO2Cob")
-
-project = rf.workspace("nathalys-workspace-mgloq").project("epp_detection-ke9vt")
-
-version = project.version(1)
-dataset = version.download("yolov8")
